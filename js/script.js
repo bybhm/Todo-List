70db98ff276188220ca8c0cee0c8d4edc445fb37
Bəhruz Mamedov <144023215+bybhm@users.noreply.github.com>
2023-10-09 17:32:00 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,65 +1,81 @@
-let todoInput;
-let todoArray = [];
+let todoinputer;
+let htmlelement;
+let todoarray = [];
 
-const addTodo = () => {
-  todoInput = document.querySelector('#todoinput').value.trim();
-  const inputContainer = document.querySelector('.inputcontainer');
-
-  if (todoInput === '') {
-    showPopup('This field is empty');
-  } else if (todoArray.includes(todoInput)) {
-    showPopup('This todo is already in your list');
-  } else if (todoArray.length >= 8) {
-    showPopup('Your todo list is full');
-  } else {
-    todoArray.push(todoInput);
-    createTodoElement(todoInput, todoArray.length - 1); 
-    console.log(todoArray.length);
-  }
-
-  document.querySelector('#todoinput').value = '';
-};
-
-const clearTodos = () => {
-  todoArray = [];
-  const todos = document.querySelectorAll('.todo');
-  todos.forEach(todo => todo.remove());
-};
-
-const removeTodo = (index) => {
-  if (index >= 0 && index < todoArray.length) {
-    todoArray.splice(index, 1);
-    const todoElement = document.querySelector(`#todo-${index}`);
-    if (todoElement) {
-      todoElement.remove();
-    }
-  }
-};
-
-const sortTodos = () => {
-  todoArray.sort();
-  const inputContainer = document.querySelector('.inputcontainer');
-  clearTodos();
-  todoArray.forEach((todo, index) => {
-    createTodoElement(todo, index);
-  });
-};
-
-const showPopup = (message) => {
-  const popupHTML = `<div class="popup">
-                        <span class="popuptext show">${message}</span>
+const todoadder = () => {
+  todoinputer = document.querySelector('#todoinput').value;
+  if (todoinputer.trim() == '' || todoinputer.trim() == null) {
+    let html = `<div class="popup">
+                        <span class="popuptext show" id="myPopup"> 
+                        This field is empty 
+                        </span>
                     </div>`;
-  document.querySelector('.inputcontainer').insertAdjacentHTML('beforeend', popupHTML);
-  setTimeout(() => {
-    document.querySelector('.popup').remove();
-  }, 1000);
+    document.querySelector('.inputcontaier').innerHTML += html;
+    setTimeout(() => {
+      document.querySelector('.popup').remove();
+    }, 1000);
+  } else if (todoarray.includes(todoinputer.trim())) {
+    let html = `<div class="popup">
+                        <span class="popuptext show" id="myPopup"> 
+                        This todo is inclue your todo
+                        </span>
+                    </div>`;
+    document.querySelector('.inputcontaier').innerHTML += html;
+    setTimeout(() => {
+      document.querySelector('.popup').remove();
+    }, 1000);
+  } else if (todoarray.length == 8) {
+    let html = `<div class="popup">
+                        <span class="popuptext show" id="myPopup"> 
+              Your todos full
+                        </span>
+                    </div>`;
+    document.querySelector('.inputcontaier').innerHTML += html;
+    setTimeout(() => {
+      document.querySelector('.popup').remove();
+    }, 1000);
+  } else {
+    todoarray.push(todoinputer.trim());
+    let html = ``;
+    todoarray.forEach((e, i) => {
+      html +=
+        e == todoinputer
+          ? `<div class='div' onclick='todoremove(${i})'>${e}</div>`
+          : '';
+    });
+    document.querySelector('.inputcontaier').innerHTML += html;
+    console.log(todoarray.length);
+  }
 };
 
-const createTodoElement = (todo, index) => {
-  const todoHTML = `<div class='todo' id='todo-${index}' onclick='removeTodo(${index})'>${todo}</div>`;
-  document.querySelector('.inputcontainer').insertAdjacentHTML('beforeend', todoHTML);
+const clearlocal = () => {
+  todoarray = [];
+
+  for (let key of document.querySelectorAll('.div')) {
+    key.remove();
+  }
+};
+const todoremove = (index) => {
+  let myupdated = todoarray.filter((e) => {
+    return e != index;
+  });
+  document.querySelectorAll('.div')[index].remove();
+  todoarray = myupdated;
+};
+const todosorter = () => {
+  todoarray.sort((a, b) => {
+    return a - b;
+  });
+  console.log(todoarray);
+  for (let key of document.querySelectorAll('.div')) {
+    key.remove();
+  }
+  let html = ``;
+  todoarray.forEach((e, i) => {
+    html += `<div class='div' onclick='todoremove(${i})'>${e}</div>`;
+  });
+  document.querySelector('.inputcontaier').innerHTML += html;
 };
 
-document.querySelector('#addTodoButton').addEventListener('click', addTodo);
-document.querySelector('#clearTodosButton').addEventListener('click', clearTodos);
-document.querySelector('#sortTodosButton').addEventListener('click', sortTodos);
+// TODO   :  this code for localstorage
+// TODO   :  this coade add enter keypress run
